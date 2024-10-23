9c2817dbb874d03c4652c591075d23ca6628ca20
Nadya <86347090+wolamey@users.noreply.github.com>
2024-10-23 23:02:17 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ const nav_link = document.querySelectorAll(".nav-link");
 
 nav_link.forEach((el) => {
   el.addEventListener("click", () => {
-    // Убираем класс 'active' у всех nav-link
+
     nav_link.forEach((link) => link.classList.remove("active"));
-    // Добавляем класс 'active' только к текущему элементу
+
     el.classList.add("active");
   });
 });
@@ -17,8 +17,8 @@ function onEntry(entry) {
 }
 
 let options = {
-  root: null, // Используйте viewport как корневой элемент
-  rootMargin: "0px", // Установите отступы, если нужно
+  root: null, 
+  rootMargin: "0px", 
 };
 
 let observer = new IntersectionObserver(onEntry, options);
@@ -28,12 +28,12 @@ for (let elm of elements) {
   observer.observe(elm);
 }
 
-// Дополнительная логика для проверки в пикселях
+
 window.addEventListener("scroll", () => {
   elements.forEach((elm) => {
     const rect = elm.getBoundingClientRect();
     if (rect.top < window.innerHeight - 500) {
-      // Например, 200 пикселей от нижней границы окна
+     
       elm.classList.add("element-show");
     }
   });
@@ -44,7 +44,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const navContainer = document.querySelector(".nav__container");
 
   toggler.addEventListener("click", function () {
-      navContainer.classList.toggle("show"); // Переключаем класс
+      navContainer.classList.toggle("show"); 
   });
 });
 
@@ -75,41 +75,39 @@ const swiper = new Swiper('.swiper', {
 
 
 
+document.addEventListener('DOMContentLoaded', function () {
+
+  emailjs.init("rLgQ3Ae2anwIKnqVj");
 
 
-  document.addEventListener('DOMContentLoaded', function () {
-    // Инициализируем EmailJS
-    emailjs.init("rLgQ3Ae2anwIKnqVj");
+  const forms = document.querySelectorAll('form');
 
-    // Находим все формы на странице
-    const forms = document.querySelectorAll('form');
+  forms.forEach((form) => {
+    form.addEventListener('submit', function (event) {
+      event.preventDefault(); 
 
-    forms.forEach((form) => {
-      form.addEventListener('submit', function (event) {
-        event.preventDefault(); // Предотвращаем стандартную отправку формы
-
-        // Собираем данные формы
-        const formData = new FormData(form);
-        const formObject = {};
-        formData.forEach((value, key) => {
-          formObject[key] = value;
-        });
-
-        // Отправляем данные через EmailJS
-        emailjs
-          .send('service_wqldakw', 'template_k4sz5bs', formObject)
-          .then(
-            function (response) {
-              console.log('SUCCESS!', response.status, response.text);
-              alert('Ваша заявка успешно отправлена!');
-              form.reset(); // Сбрасываем форму
-            },
-            function (error) {
-              console.log('FAILED...', error);
-              alert('Произошла ошибка при отправке заявки.');
-            }
-          );
+    
+      const formData = new FormData(form);
+      const formObject = {};
+      formData.forEach((value, key) => {
+        formObject[key] = value;
       });
+
+
+  
+      emailjs
+        .send('service_wqldakw', 'template_k4sz5bs', formObject)
+        .then(
+          function (response) {
+            console.log('SUCCESS!', response.status, response.text);
+            alert('Ваша заявка успешно отправлена!');
+            form.reset();
+          },
+          function (error) {
+            console.log('FAILED...', error);
+            alert('Произошла ошибка при отправке заявки.');
+          }
+        );
     });
   });
-
+});
