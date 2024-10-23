d8f79182b459d75907dc8956dd839f484b3e0d23
overschool2 <btechologics@mail.ru>
2024-10-23 22:42:35 +0300

    slider, form

--- script.js
+++ script.js
@@ -47,3 +47,69 @@ document.addEventListener("DOMContentLoaded", function () {
       navContainer.classList.toggle("show"); // Переключаем класс
   });
 });
+
+
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  // If we need pagination
+  // pagination: {
+  //   el: '.swiper-pagination',
+  // },
+
+  // Navigation arrows
+  // navigation: {
+  //   nextEl: '.swiper-button-next',
+  //   prevEl: '.swiper-button-prev',
+  // },
+
+  // And if we need scrollbar
+  // scrollbar: {
+  //   el: '.swiper-scrollbar',
+  // },
+});
+
+
+
+
+
+
+  document.addEventListener('DOMContentLoaded', function () {
+    // Инициализируем EmailJS
+    emailjs.init("rLgQ3Ae2anwIKnqVj");
+
+    // Находим все формы на странице
+    const forms = document.querySelectorAll('form');
+
+    forms.forEach((form) => {
+      form.addEventListener('submit', function (event) {
+        event.preventDefault(); // Предотвращаем стандартную отправку формы
+
+        // Собираем данные формы
+        const formData = new FormData(form);
+        const formObject = {};
+        formData.forEach((value, key) => {
+          formObject[key] = value;
+        });
+
+        // Отправляем данные через EmailJS
+        emailjs
+          .send('service_wqldakw', 'template_k4sz5bs', formObject)
+          .then(
+            function (response) {
+              console.log('SUCCESS!', response.status, response.text);
+              alert('Ваша заявка успешно отправлена!');
+              form.reset(); // Сбрасываем форму
+            },
+            function (error) {
+              console.log('FAILED...', error);
+              alert('Произошла ошибка при отправке заявки.');
+            }
+          );
+      });
+    });
+  });
+
